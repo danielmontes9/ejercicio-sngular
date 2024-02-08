e2f354b3cab78389f3f53f2e5bfbc879a31a4df9
Daniel Montes <gdanielmontesh9@gmail.com>
2024-02-08 12:50:54 -0600

    serie triangular y primo

--- src/app/shared/utils/series.ts
+++ src/app/shared/utils/series.ts
@@ -1,6 +1,4 @@
-
-
-// FUNCION QUE RETORNA EL DIGITO N DE LA SERIE FIBONACCI
+//* FUNCION QUE RETORNA EL DIGITO N DE LA SERIE FIBONACCI
 function fibonacci(n: number) {
     let a = 0;
     let b = 1;
@@ -19,11 +17,44 @@ function fibonacci(n: number) {
 }
 
 
+//* FUNCION QUE RETORNA EL DIGITO N DE LOS NUMEROS TRIANGULARES
 function triangular(n: number) {
-    return n;
+    if (n <= 0) return 0;
+    return (n * (n + 1)) / 2;
 }
 
 
-function primo(n: number) {
-    return n;
+//* FUNCION QUE RETORNA EL DIGITO N DE LOS NUMEROS PRIMOS
+function nthPrimo(n) {
+    if (n === 1) return 2; // El primer número primo es 2
+    let count = 1;
+    let numero = 3; // Comenzamos desde el segundo número primo, que es 3
+
+    while (count < n) {
+        if (esPrimo(numero)) {
+            count++;
+        }
+        if (count === n) {
+            return numero;
+        }
+        numero += 2; // Solo consideramos números impares como primos
+    }
+
+    return -1; // Retornar -1 si no se encuentra el término n
+}
+
+//* FUNCION PARA DETERMINAR SI UN NUMERO ES PRIMO
+function esPrimo(numero) {
+    if (numero <= 1) return false;
+    if (numero <= 3) return true;
+
+    if (numero % 2 === 0 || numero % 3 === 0) return false;
+
+    let i = 5;
+    while (i * i <= numero) {
+        if (numero % i === 0 || numero % (i + 2) === 0) return false;
+        i += 6;
+    }
+
+    return true;
 }
