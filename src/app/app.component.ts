5ebd87f5fbd033d64d69c17d8b65aba6754ab05e
Daniel Montes <gdanielmontesh9@gmail.com>
2024-02-08 09:51:41 -0600

    add formcontrol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ejercicio-sngular';
+  numberNFormControl = new FormControl('', [Validators.required]);
+
+  ngOnInit() {
+    console.log(this.numberNFormControl)
+  }
+
 }
