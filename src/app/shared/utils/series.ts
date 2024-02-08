c56b98d4cd3c9ecd7aeebbb9ec2012a93c6acbdb
Daniel Montes <gdanielmontesh9@gmail.com>
2024-02-08 12:42:32 -0600

    serie fibonacci

--- src/app/shared/utils/series.ts
+++ src/app/shared/utils/series.ts
@@ -1,8 +1,21 @@
 
 
-
+// FUNCION QUE RETORNA EL DIGITO N DE LA SERIE FIBONACCI
 function fibonacci(n: number) {
-    return n;
+    let a = 0;
+    let b = 1;
+    let temp;
+
+    if (n === 0) return a;
+    if (n === 1) return b;
+
+    for (let i = 2; i <= n; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    return b;
 }
 
 
